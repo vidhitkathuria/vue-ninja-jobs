0c4257dbaa663db92810b646917b629691b4625d
vidhitkathuria <72202024+vidhitkathuria@users.noreply.github.com>
2021-06-29 18:49:00 +0530

    added files

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+      perspiciatis velit vel aperiam aliquam, modi dolorum consequatur
+      distinctio illum quis assumenda nesciunt, animi aut beatae molestias
+      incidunt consequuntur magnam sint!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima
+      perspiciatis velit vel aperiam aliquam, modi dolorum consequatur
+      distinctio illum quis assumenda nesciunt, animi aut beatae molestias
+      incidunt consequuntur magnam sint!
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
